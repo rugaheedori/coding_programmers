b6a3b575b44872a1a52c506f72e6d862eebd583c
rugaheedori <qufslarkgml@gmail.com>
2021-12-18 23:07:25 +0900

    gahee : 다음-큰-숫자 풀이 & solution함수 재귀함수 사용법, 함수 매개변수 활용 법

--- programmers/다음-큰-숫자.js
+++ programmers/다음-큰-숫자.js
@@ -18,4 +18,16 @@ function solution(n) {
   return n;
 }
 
-console.log(solution(78));
+// solution함수를 재귀함수로 이용하는 방법 (인자를 추가적 선언 가능)
+// function solution(n, a = n + 1) {
+//   return n.toString(2).match(/1/g).length == a.toString(2).match(/1/g).length
+//     ? a
+//     : solution(n, a + 1);
+// } => a는 정의되지 않았기 때문에 undefined => 초기 설정인 n + 1이 됨
+// 함수 사용 시 매개변수의 인자값이 undefined라면, 함수 정의 시의 매개변수 초기값으로 설정된다.
+// const func = (a, b = 2) => {
+//   console.log(a * b);
+// };
+
+// func(3, undefined); // 6
+// func(3, 10); //30
