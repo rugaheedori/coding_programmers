ebefdf47708f8d5159f0e3a2c88d54768ab1ab83
rugaheedori <qufslarkgml@gmail.com>
2022-01-30 21:32:44 +0900

    Refactor : 주차-요금-계산 문제 풀이 수정

--- programmers/주차-요금-계산.js
+++ programmers/주차-요금-계산.js
@@ -1,40 +1,24 @@
 function solution(fees, records) {
   const answer = {};
-  const times = {};
 
   records.forEach((record) => {
-    const data = record.split(' ');
-    if (data[2] === 'IN') {
-      answer[data[1]] = data[0];
-      if (!Object.keys(times).includes(data[1])) times[data[1]] = 0;
+    const [time, car, type] = record.split(' ');
+    const [hour, minutes] = time.split(':');
+    if (!answer[car]) answer[car] = 0;
+    if (type === 'IN') {
+      answer[car] += 1439 - (hour * 60 + minutes * 1);
     } else {
-      times[data[1]] += getMinutes(answer[data[1]], data[0]);
-      delete answer[data[1]];
+      answer[car] -= 1439 - (hour * 60 + minutes * 1);
     }
   });
 
-  for (let car in answer) {
-    times[car] += getMinutes(answer[car], '23:59');
-  }
-
-  const result = Object.keys(times)
+  const result = Object.keys(answer)
     .sort((a, b) => a - b)
-    .map((time) => {
-      return calculationFee(times[time], fees);
+    .map((car) => {
+      return answer[car] - fees[0] > 0
+        ? fees[1] + Math.ceil((answer[car] - fees[0]) / fees[2]) * fees[3]
+        : fees[1];
     });
 
   return result;
-}
-
-function getMinutes(inTime, outTime) {
-  const inTimes = inTime.split(':');
-  const outTimes = outTime.split(':');
-
-  return (outTimes[0] - inTimes[0]) * 60 + (outTimes[1] - inTimes[1]) * 1;
-}
-
-function calculationFee(time, fees) {
-  return time - fees[0] > 0
-    ? fees[1] + Math.ceil((time - fees[0]) / fees[2]) * fees[3]
-    : fees[1];
 }
