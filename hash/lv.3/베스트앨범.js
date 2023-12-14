68d0eccef86595dae7474b63075be6c2e023dc57
hee <qufslarkgml@gmail.com>
2023-12-14 23:54:45 +0900

    hash: 베스트앨범 풀이 추가

--- hash/lv.3/베스트앨범.js
+++ hash/lv.3/베스트앨범.js
@@ -25,3 +25,38 @@ function solution(genres, plays) {
     .split(",")
     .map((v) => +v);
 }
+
+function solution(genres, plays) {
+  const answer = [];
+  const genreMap = new Map();
+
+  genres.forEach((x, idx) => {
+    if (genreMap.get(x)) {
+      let { total, best } = genreMap.get(x);
+
+      if (plays[idx] > plays[best[0]]) {
+        best = [idx, best[0]];
+      } else if (plays[best[0]] >= plays[idx] && plays[idx] > plays[best[1]]) {
+        best = [best[0], idx];
+      } else {
+        best.push(idx);
+
+        if (best.length > 2) {
+          best.pop();
+        }
+      }
+
+      genreMap.set(x, { total: total + plays[idx], best });
+    } else {
+      genreMap.set(x, { total: plays[idx], best: [idx] });
+    }
+  });
+
+  Array.from(genreMap)
+    .sort((a, b) => b[1].total - a[1].total)
+    .forEach((x) => {
+      answer.push(...x[1].best);
+    });
+
+  return answer;
+}
