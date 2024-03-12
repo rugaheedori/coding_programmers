f479079889270329ae6073088e473d025147ef90
hee <qufslarkgml@gmail.com>
2024-03-12 16:24:15 +0900

    택배-배달과-수거하기 풀이

--- uncategorized/lv.2/택배-배달과-수거하기.js
+++ uncategorized/lv.2/택배-배달과-수거하기.js
@@ -1,36 +1,49 @@
 function solution(cap, n, deliveries, pickups) {
   let answer = 0;
-  let trunk = [0, 0];
 
-  while (!trunk[0] && !trunk[1]) {
-    for (let i = n - 1; i >= 0; i--) {
-      let delivery = deliveries[i];
-      let pickup = pickups[i];
+  while (deliveries.length || pickups.length) {
+    let truck = [0, 0];
+    let path = [0, 0];
 
-      if ((delivery > 0 || pickup > 0) && !trunk[0] && !trunk[1]) {
-        answer += (i + 1) * 2;
+    for (let i = deliveries.length - 1; i >= 0; i--) {
+      if (deliveries[i] && !truck[0]) {
+        path[0] = i + 1; // 제일 멀리 배달온 거리
       }
 
-      if (delivery) {
-        if (trunk[0] + delivery >= cap) {
-          deliveries[i] -= cap - trunk[0];
-          trunk[0] = cap;
-        } else {
-          trunk[0] += delivery;
-          deliveries[i] = 0;
-        }
+      if (deliveries[i] >= cap - truck[0]) {
+        deliveries[i] -= cap - truck[0];
+        truck[0] = cap;
+        break;
+      } else {
+        truck[0] += deliveries[i];
+        deliveries[i] = 0;
+      }
+    }
+
+    for (let j = pickups.length - 1; j >= 0; j--) {
+      if (pickups[j] && !truck[1]) {
+        path[1] = j + 1; // 제일 멀리 수거한 거리
       }
 
-      if (pickup) {
-        if (trunk[1] + pickup >= cap) {
-          pickups[i] -= cap - trunk[1];
-          trunk = [0, 0];
-          break;
-        } else {
-          pickups[i] = 0;
-          trunk[1] += pickup;
-        }
+      if (pickups[j] >= cap - truck[1]) {
+        pickups[j] -= cap - truck[1];
+        truck[1] = cap;
+        break;
+      } else {
+        truck[1] += pickups[j];
+        pickups[j] = 0;
       }
+    }
+
+    answer += Math.max(...path) * 2;
+
+    // 배달 완료 & 수거 완료한 끝 지역 삭제
+    while (!deliveries[deliveries.length - 1] && deliveries.length) {
+      deliveries.pop();
+    }
+
+    while (!pickups[pickups.length - 1] && pickups.length) {
+      pickups.pop();
     }
   }
 
