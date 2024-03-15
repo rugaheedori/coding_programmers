6c4378a6e8cc79a74b9bb6d787475bc129df1197
hee <qufslarkgml@gmail.com>
2024-03-15 18:26:02 +0900

    k진수에서-소수-개수-구하기 풀이

--- uncategorized/lv.2/k진수에서-소수-개수-구하기.js
+++ uncategorized/lv.2/k진수에서-소수-개수-구하기.js
@@ -1,24 +1,21 @@
 function solution(n, k) {
-  let convertNum = "";
   let answer = 0;
 
-  while (n) {
-    convertNum = (n % k) + convertNum;
-    n = (n - (n % k)) / k;
-  }
-
-  convertNum.split("0").forEach((x) => {
-    if (x >= 2) {
-      for (let i = 2; i <= Math.sqrt(x); i++) {
-        if (x % i === 0) {
-          answer -= 1;
-          break;
+  // toString() => 진수변환 제공
+  n.toString(k)
+    .split("0")
+    .forEach((x) => {
+      if (x >= 2) {
+        for (let i = 2; i <= Math.sqrt(x); i++) {
+          if (x % i === 0) {
+            answer -= 1;
+            break;
+          }
         }
-      }
 
-      answer += 1;
-    }
-  });
+        answer += 1;
+      }
+    });
 
   return answer;
 }
