d2f428c082d7dfdd695b248724deb1feefcfc10b
hee <qufslarkgml@gmail.com>
2024-03-18 23:56:54 +0900

    땅따먹기 풀이

---
+++ uncategorized/lv.2/땅따먹기.js
@@ -0,0 +1,19 @@
+function solution(land) {
+  const landLen = land.length;
+
+  for (var i = 0; i < land.length - 1; i++) {
+    land[i + 1][0] += Math.max(land[i][1], land[i][2], land[i][3]);
+    land[i + 1][1] += Math.max(land[i][0], land[i][2], land[i][3]);
+    land[i + 1][2] += Math.max(land[i][0], land[i][1], land[i][3]);
+    land[i + 1][3] += Math.max(land[i][0], land[i][1], land[i][2]);
+  }
+
+  const maxSum = Math.max(
+    land[landLen - 1][0],
+    land[landLen - 1][1],
+    land[landLen - 1][2],
+    land[landLen - 1][3]
+  );
+
+  return maxSum;
+}
