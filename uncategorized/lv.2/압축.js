97a0fc0ef98cf81d0fe4a865f7a4bafe01a2d8a4
hee <qufslarkgml@gmail.com>
2024-04-04 00:29:50 +0900

    압축 풀이

--- uncategorized/lv.2/압축.js
+++ uncategorized/lv.2/압축.js
@@ -30,18 +30,23 @@ function solution(msg) {
 
 function solution(msg) {
   const dict = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("").reduce((dict, c, i) => {
-    dict[c] = i + 1;
+    dict.set(c, i + 1);
+
     return dict;
-  }, {});
+  }, new Map());
+
   dict.nextId = 27;
-  const ans = [];
+
+  const answer = [];
+
   for (let i = 0, j = 0; i < msg.length; i = j) {
     j = msg.length;
-    let longest = "";
-    while (dict[(longest = msg.substring(i, j))] === undefined) --j;
-    ans.push(dict[longest]);
-    dict[longest + msg[j]] = dict.nextId++;
+
+    while (!dict.has(msg.slice(i, j))) --j;
+
+    answer.push(dict.get(msg.slice(i, j)));
+    dict.set(msg.slice(i, j) + msg[j], dict.nextId++);
   }
 
-  return ans;
+  return answer;
 }
