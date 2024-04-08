4f64eeb4d65505f911fb8a3880969978a03fa7bd
hee <qufslarkgml@gmail.com>
2024-04-08 23:38:39 +0900

    할인-행사 풀이

--- uncategorized/lv.2/할인-행사.js
+++ uncategorized/lv.2/할인-행사.js
@@ -3,30 +3,23 @@ function solution(want, number, discount) {
   let result = 0;
 
   function checking() {
-    let check = true;
-
     for (let i = 0; i < want.length; i++) {
       if (map.get(want[i]) !== number[i]) {
-        check = false;
-        break;
+        return;
       }
     }
+    result++;
+  }
 
-    if (check) {
-      result++;
+  for (let i = 0; i < discount.length; i++) {
+    if (i >= 10) {
+      const outItem = discount[i - 10];
+
+      map.set(outItem, map.get(outItem) - 1);
     }
-  }
 
-  for (let i = 0; i < 10; i++) {
-    map.set(discount[i], (map.get(discount[i]) || 0) + 1);
-  }
-  checking();
-
-  for (let i = 10; i < discount.length; i++) {
-    const outItem = discount[i - 10];
     const inItem = discount[i];
 
-    map.set(outItem, map.get(outItem) - 1);
     map.set(inItem, (map.get(inItem) || 0) + 1);
     checking();
   }
