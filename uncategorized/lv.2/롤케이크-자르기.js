10b496fbfa7409fc0d48218114bdd369598713a0
hee <qufslarkgml@gmail.com>
2024-04-21 23:59:36 +0900

    롤케이크-자르기 풀이

--- uncategorized/lv.2/롤케이크-자르기.js
+++ uncategorized/lv.2/롤케이크-자르기.js
@@ -29,3 +29,30 @@ function solution(topping) {
 
   return answer;
 }
+
+function solution(topping) {
+  const toppingSet = new Set();
+  const toppingMap = new Map();
+
+  let answer = 0;
+  let check = 0;
+
+  for (let i = 0; i < topping.length; i++) {
+    if (toppingMap.get(topping[i])) {
+      toppingMap.set(topping[i], toppingMap.get(topping[i]) + 1);
+    } else {
+      toppingMap.set(topping[i], 1);
+      check++;
+    }
+  }
+
+  for (let i = 0; i < topping.length; i++) {
+    toppingSet.add(topping[i]);
+    toppingMap.set(topping[i], toppingMap.get(topping[i]) - 1);
+
+    if (!toppingMap.get(topping[i])) check--;
+    if (toppingSet.size === check) answer++;
+  }
+
+  return answer;
+}
