3d8b9b0c8904b015fee69ec46daabe0228581581
hee <qufslarkgml@gmail.com>
2024-05-06 20:35:52 +0900

    queue: 두-큐의-합-같게-만들기 풀이

---
+++ queue/lv.2/두-큐의-합-같게-만들기.js
@@ -0,0 +1,31 @@
+function solution(queue1, queue2) {
+  let sum1 = queue1.reduce((a, b) => a + b, 0);
+  let sum2 = queue2.reduce((a, b) => a + b, 0);
+  const avg = (sum1 + sum2) / 2;
+
+  // 총 합이 홀수인 경우
+  if (avg % 1) return -1;
+
+  let answer = 0;
+  let startIdx = 0;
+  let endIdx = queue1.length - 1;
+
+  const queue = sum1 > sum2 ? [...queue1, ...queue2] : [...queue2, ...queue1];
+  let maxSum = sum1 > sum2 ? sum1 : sum2;
+
+  while (answer < queue1.length * 3) {
+    if (maxSum === avg) return answer;
+
+    answer++;
+
+    if (maxSum > avg) {
+      maxSum -= queue[startIdx];
+      startIdx = (startIdx + 1) % (queue1.length * 2);
+    } else if (maxSum < avg) {
+      endIdx = (endIdx + 1) % (queue1.length * 2);
+      maxSum += queue[endIdx];
+    }
+  }
+
+  return -1;
+}
